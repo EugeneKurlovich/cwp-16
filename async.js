cd06f3e71ed1d59016dcd35bf3f878f02816c75a
EugeneKurlovich <zhenikpggkurlovich@gmail.com>
2017-12-06 04:16:03 +0300

    get info about Minsk, Madrid, Rome

--- async.js
+++ async.js
@@ -1,6 +1,27 @@
 const axios = require('axios');
-
+const bluebird = require('bluebird');
 
 axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=Minsk`).then(function(response){
 console.log(response.data.results[0].formatted_address);
 });
+
+
+
+start();
+
+async function start()
+{
+    let cityCountry = await bluebird.all(
+        [
+            axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=Minsk`),
+            axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=Madrid`),
+            axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=Rome`)
+        ]
+    );
+
+    cityCountry.forEach((i)=>
+    {
+        console.log(i.data.results[0].formatted_address);
+    });
+
+}
