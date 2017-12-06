00695f5a804072f5a7bdbe1d332a19f079e97796
EugeneKurlovich <zhenikpggkurlovich@gmail.com>
2017-12-06 04:51:24 +0300

    Nearest city

--- co.js
+++ co.js
@@ -19,4 +19,24 @@ async function start()
                                                                                 value[1].data.results[0].geometry.location));
     });
 
+
+    await co(function* () {
+        return yield Promise.mapSeries(
+            [
+            `https://maps.googleapis.com/maps/api/geocode/json?address=Minsk`,
+            `https://maps.googleapis.com/maps/api/geocode/json?address=Copenhagen`,
+            `https://maps.googleapis.com/maps/api/geocode/json?address=Oslo`,
+            `https://maps.googleapis.com/maps/api/geocode/json?address=Brussel`
+        ], async function (item) {
+            return await axios.get(item);
+        });
+    }).then(function (value) {
+        let cities = {};
+        value.map((city) => {
+            cities[city.data.results[0].formatted_address] = city.data.results[0].geometry.location;
+        });
+        console.log("Ближайший к Минску : " + geolib.findNearest(cities['Minsk, Belarus'], cities, 1));
+    }, function (err) {
+        console.error(err.stack);
+    });
 }
