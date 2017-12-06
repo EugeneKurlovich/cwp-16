08a5ae5b9014d807cb428cfc4c0e712cad96d87e
EugeneKurlovich <zhenikpggkurlovich@gmail.com>
2017-12-06 04:39:25 +0300

    Distance between Brest and Minsk

--- co.js
+++ co.js
@@ -7,5 +7,16 @@ start();
 
 async function start()
 {
+    await co(function* () 
+    {
+        return yield Promise.all(
+        [
+            axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=Brest`),
+            axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=Minsk`)
+        ]);
+    }).then(function (value) {
+        console.log("Расстояние между Брестом и Минском : " + geolib.getDistance(value[0].data.results[0].geometry.location,
+                                                                                value[1].data.results[0].geometry.location));
+    });
 
 }
