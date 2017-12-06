44563f0b31963a6c59fe04c0ebe8b26162fd1f28
EugeneKurlovich <zhenikpggkurlovich@gmail.com>
2017-12-06 05:05:33 +0300

    info about street and all components

--- co.js
+++ co.js
@@ -33,10 +33,21 @@ async function start()
     }).then(function (value) {
         let cities = {};
         value.map((city) => {
+            console.log(city.data.results[0].formatted_address + "                     " + city.data.results[0].geometry.location);
             cities[city.data.results[0].formatted_address] = city.data.results[0].geometry.location;
         });
         console.log("Ближайший к Минску : " + geolib.findNearest(cities['Minsk, Belarus'], cities, 1));
-    }, function (err) {
-        console.error(err.stack);
+    });
+
+
+    await co(function* () {
+        return yield Promise.all([
+            axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=Piazza del Сolosseo`)
+        ]);
+    }).then(function (value) {
+        console.log(value[0].data.results[0].formatted_address);
+        value[0].data.results[0].address_components.forEach((component) => {
+            console.log(`   -${component.long_name}`);
+        });
     });
 }
